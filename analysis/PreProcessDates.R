fc176b6a34139643f875faccac490356f5bc714f
John <john.boudreaux.93@gmail.com>
2018-07-24 06:38:52 -0400

    progress on R script for rep/dems

--- analysis/PreProcessDates.R
+++ analysis/PreProcessDates.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(dpylr)
 
 lyrics <- read.csv("C:/Users/johnb/Documents/MIDS/W200_Python/PythonProj2/data/HipHopLyrics.csv")
 reps <- read.csv("C:/Users/johnb/Documents/MIDS/W200_Python/PythonProj2/data/republicans2016.csv")
@@ -29,8 +30,27 @@ process.dates <- function(input.date.vect){
     }
     input.date.vect[i] = paste0(input.date.vect[i], "/", current.year)
   }
+  input.date.vect <- gsub("/", "-", input.date.vect)
   return(input.date.vect)
 }
 
-reps$Date <- process.dates(reps$Date)
-dems$Date <- process.dates(dems$Date)
+reps$Date <- as.POSIXct(process.dates(reps$Date), tz = "UTC", format = "%m-%d-&Y")
+dems$Date <- as.POSIXct(process.dates(dems$Date), tz = "UTC", format = "%m-%d-&Y")
+
+min.clinton <- min(dems$Date[!is.na(dems$Clinton)])
+max.clinton <- max(dems$Date[!is.na(dems$Clinton)])
+min.bernie <- min(dems$Date[!is.na(dems$Sanders)])
+max.bernie <- min(dems$Date[!is.na(dems$Sanders)])
+min.trump <- min(reps$Date)[!is.na(reps$Trump)]
+max.trump <- max(reps$Date)[!is.na(reps$Trump)]
+min.cruz <- min(reps$Date)[!is.na(reps$Cruz)]
+max.cruz <- max(reps$Date)[!is.na(reps$Cruz)]
+min.kasich <- min(reps$Date)[!is.na(reps$Kasich)]
+max.kasich <- max(reps$Date)[!is.na(reps$Kasich)]
+
+len.clinton <- max.clinton - min.clinton
+len.bernie <- max.bernie - min.bernie
+len.trump <- max.trump - min.trump
+len.cruz <- max.cruz - min.cruz
+len.kasich <- max.kasich - min.kasich
+
